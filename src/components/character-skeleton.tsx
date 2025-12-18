7b20bb79c1695a1a490e5f7f045ac3c7e6e5b647
unknown <zul.developer.io>
2025-12-18 20:02:38 +0100

    fix

--- src/components/character-skeleton.tsx
+++ src/components/character-skeleton.tsx
@@ -1,6 +1,6 @@
 export const CharacterCardSkeleton = () => {
   return (
-    <div className="bg-zinc-900 rounded-2xl animate-pulse h-40 flex flex-col w-full max-w-xs">
+    <div className="bg-zinc-900 rounded-2xl animate-pulse h-40 flex flex-col w-full max-w-md">
       <div className="flex items-center justify-center h-12 rounded-t-2xl bg-blue-700 text-white text-lg font-semibold shrink-0" />
 
       <div className="flex-1 px-4 py-3 space-y-2 overflow-hidden">
